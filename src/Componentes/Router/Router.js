87b76ee1a1afd9eb2c221eef46466dfed134b9e1
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-04-22 08:03:37 -0500

    Actualización desarrollo

--- src/Componentes/Router/Router.js
+++ src/Componentes/Router/Router.js
@@ -3,48 +3,16 @@ import {BrowserRouter, Switch, Route, Redirect, } from "react-router-dom";
 import {masterPage} from '../../Shared/masterPage';
 
 import Home from '../Containers/Home/Home';
-import Administrator from '../Containers/Administrador/Administrator/Administrator';
-
 import Usuarios from '../Containers/Administrador/Usuarios/Usuarios';
-import Dispositivos from '../Containers/Administrador/Dispositivos/Dispositivos';
-import Microservicios from '../Containers/Administrador/Microservicios/Microservicios';
-import Modulos from '../Containers/Administrador/Modulos/Modulos';
-import Roles from '../Containers/Administrador/Roles/Roles';
-import ConfiguracionMicroserviciosModulos from '../Containers/Administrador/ConfiguracionMicroserviciosModulos/ConfiguracionMicroserviciosModulos';
-import ConfiguracionUsuarios from '../Containers/Administrador/ConfiguracionUsuarios/ConfiguracionUsuarios';
-
-import NodoSensores from '../Containers/Administrador/Sensores/NodoSensores/NodoSensores';
-import VariablesNodoSensor from '../Containers/Administrador/Sensores/VariablesNodoSensor/VariablesNodoSensor';
-import ConfiguracionVariablesNodo from '../Containers/Administrador/Sensores/ConfiguracionVariablesNodo/ConfiguracionVariablesNodo';
-import ConfiguracionReglas from '../Containers/Administrador/Sensores/ConfiguracionReglas/ConfiguracionReglas';
-import DatosNodoSensor from '../Containers/Administrador/Sensores/DatosNodoSensor/DatosNodoSensor';
-
 class Router extends Component {
-
-
   //El metodo de redireccionamiento. <Redirect from="/" to="/home" />
-
   render () {
     return (
       <div>
         <BrowserRouter>
           <Switch>
             <Route exact path="/Home" component={Home}/>
-            <Route exact path="/Administrator" component={masterPage(Administrator)}/>
-
             <Route exact path="/Administrator/Authentication/Usuarios" component={masterPage(Usuarios)}/>
-            <Route exact path="/Administrator/Authentication/Dispositivos" component={masterPage(Dispositivos)}/>
-            <Route exact path="/Administrator/Authentication/Microservicios" component={masterPage(Microservicios)}/>
-            <Route exact path="/Administrator/Authentication/Modulos" component={masterPage(Modulos)}/>
-            <Route exact path="/Administrator/Authentication/Roles" component={masterPage(Roles)}/>
-            <Route exact path="/Administrator/Authentication/ConfiguracionMSM" component={masterPage(ConfiguracionMicroserviciosModulos)}/>
-            <Route exact path="/Administrator/Authentication/ConfiguracionUsuarios" component={masterPage(ConfiguracionUsuarios)}/>
-
-            <Route exact path="/Administrator/Sensores/NodoSensores" component={masterPage(NodoSensores)}/>
-            <Route exact path="/Administrator/Sensores/VariablesNodoSensor" component={masterPage(VariablesNodoSensor)}/>
-            <Route exact path="/Administrator/Sensores/ConfiguracionVNS" component={masterPage(ConfiguracionVariablesNodo)}/>
-            <Route exact path="/Administrator/Sensores/ConfiguracionRNS" component={masterPage(ConfiguracionReglas)}/>
-            <Route exact path="/Administrator/Sensores/DatosNodoSensor" component={masterPage(DatosNodoSensor)}/>
             <Redirect to ="/Home"/>
           </Switch>
         </BrowserRouter>
--- src/Shared/masterPage.js
+++ src/Shared/masterPage.js
@@ -87,43 +87,42 @@ export const masterPage =  (ComposedComponent) => {
         
                     <Container>
                         <Header>
-                            <Nav  style={{backgroundColor:'#fff', height:'60px', display:'fixed'}}>
-                                <div className="navbar_container">
-                                <div className='sidebar-container'>
-                                    <div className='content-left'>
-                                        <Nav pullRight>
-                                            <Nav.Item onClick={this.handleToggle}  style={{ width: 56, textAlign: 'center' }}>
-                                                <Icon icon={expand ? 'angle-left' : 'bars'}  style={{color: '#1d43ad'}}/>
-                                            </Nav.Item>
-                                        </Nav>
-                                    </div>
-                                    <div className='content-right'>
-                                        <div className='right-container'>
-                                        <svg 
-                                            xmlns="http://www.w3.org/2000/svg" 
-                                            width="40" height="40" viewBox="0 0 32 32"
-                                        >
-                                            <g transform="translate(-1093 -16.291)">
-                                            <circle style={{fill:'#E7E7EE'}} cx="16" cy="16" r="16" transform="translate(1093 16.291)"/>
-                                            <path style={{fill:'#3b3798'}} d="M133.942,21.313a3.949,3.949,0,1,0-3.955-3.955A3.956,3.956,0,0,0,133.942,21.313Zm0-6.867a2.912,2.912,0,1,1-2.912,2.912A2.92,2.92,0,0,1,133.942,14.446Z" transform="translate(974.749 11.139)"/><path className="b" d="M.522,346.41H14.256a.518.518,0,0,0,.522-.522,4.968,4.968,0,0,0-4.962-4.962H4.962A4.968,4.968,0,0,0,0,345.888.518.518,0,0,0,.522,346.41Zm4.441-4.441H9.815a3.916,3.916,0,0,1,3.882,3.4H1.08A3.927,3.927,0,0,1,4.962,341.969Z" transform="translate(1101.326 -307.782)"/></g></svg>
-                                        <div>
-                                            <p className='nombre-user'>Prueba</p>
+                            <Nav style={{height:'60px', display:'fixed'}}>
+                                <div className="navbarcontainer">
+                                    <div className='sidebar-container'>
+                                        <div className='content-left'>
+                                            <Nav pullRight>
+                                                <Nav.Item onClick={this.handleToggle}  style={{ width: 56, textAlign: 'center' }}>
+                                                    <Icon icon={expand ? 'angle-left' : 'bars'}  style={{color: '#1d43ad'}}/>
+                                                </Nav.Item>
+                                            </Nav>
                                         </div>
-                                        <svg 
-                                            onClick={() => {
-                                            LoginAction_CerrarSesion();
-                                                // history.push('/Home')
-                                            }}  
-                                            xmlns="http://www.w3.org/2000/svg" 
-                                            width="30" 
-                                            height="30"
-                                            viewBox="0 0 24 24" 
-                                            style={{fill:'#E7E7EE'}}><g transform="translate(0 -0.85)"><g transform="translate(0 0.85)"><g transform="translate(0 0)"><path className="a" d="M11.444,22.85H2.861a.978.978,0,0,1-.954-1v-18a.978.978,0,0,1,.954-1h8.583a.977.977,0,0,0,.954-1,.977.977,0,0,0-.954-1H2.861A2.937,2.937,0,0,0,0,3.85v18a2.937,2.937,0,0,0,2.861,3h8.583a1,1,0,0,0,0-2Z" transform="translate(0 -0.85)"/></g></g><g transform="translate(8.665 6.174)"><path className="a" d="M185.151,113.161l-5.8-5.722a.954.954,0,0,0-1.339,1.358l4.144,4.089h-11.1a.954.954,0,0,0,0,1.907h11.1l-4.144,4.089a.954.954,0,1,0,1.339,1.358l5.8-5.722a.953.953,0,0,0,0-1.358Z" transform="translate(-170.1 -107.165)"/></g></g></svg>
+                                        <div className='content-right'>
+                                            <div className='right-container'>
+                                            <svg 
+                                                xmlns="http://www.w3.org/2000/svg" 
+                                                width="40" height="40" viewBox="0 0 32 32"
+                                            >
+                                                <g transform="translate(-1093 -16.291)">
+                                                <circle style={{fill:'#E7E7EE'}} cx="16" cy="16" r="16" transform="translate(1093 16.291)"/>
+                                                <path style={{fill:'#3b3798'}} d="M133.942,21.313a3.949,3.949,0,1,0-3.955-3.955A3.956,3.956,0,0,0,133.942,21.313Zm0-6.867a2.912,2.912,0,1,1-2.912,2.912A2.92,2.92,0,0,1,133.942,14.446Z" transform="translate(974.749 11.139)"/><path className="b" d="M.522,346.41H14.256a.518.518,0,0,0,.522-.522,4.968,4.968,0,0,0-4.962-4.962H4.962A4.968,4.968,0,0,0,0,345.888.518.518,0,0,0,.522,346.41Zm4.441-4.441H9.815a3.916,3.916,0,0,1,3.882,3.4H1.08A3.927,3.927,0,0,1,4.962,341.969Z" transform="translate(1101.326 -307.782)"/></g></svg>
+                                            <div>
+                                                <p className='nombre-user'>Prueba</p>
+                                            </div>
+                                            <svg 
+                                                onClick={() => {
+                                                LoginAction_CerrarSesion();
+                                                    // history.push('/Home')
+                                                }}  
+                                                xmlns="http://www.w3.org/2000/svg" 
+                                                width="30" 
+                                                height="30"
+                                                viewBox="0 0 24 24" 
+                                                style={{fill:'#E7E7EE'}}><g transform="translate(0 -0.85)"><g transform="translate(0 0.85)"><g transform="translate(0 0)"><path className="a" d="M11.444,22.85H2.861a.978.978,0,0,1-.954-1v-18a.978.978,0,0,1,.954-1h8.583a.977.977,0,0,0,.954-1,.977.977,0,0,0-.954-1H2.861A2.937,2.937,0,0,0,0,3.85v18a2.937,2.937,0,0,0,2.861,3h8.583a1,1,0,0,0,0-2Z" transform="translate(0 -0.85)"/></g></g><g transform="translate(8.665 6.174)"><path className="a" d="M185.151,113.161l-5.8-5.722a.954.954,0,0,0-1.339,1.358l4.144,4.089h-11.1a.954.954,0,0,0,0,1.907h11.1l-4.144,4.089a.954.954,0,1,0,1.339,1.358l5.8-5.722a.953.953,0,0,0,0-1.358Z" transform="translate(-170.1 -107.165)"/></g></g></svg>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                                </div>
-                                
                             </Nav>
                         </Header>
                         <Content>
