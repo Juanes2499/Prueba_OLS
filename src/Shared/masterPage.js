54733239b7f73765bd3d7f766012194eb6fe1ee0
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-04-22 02:02:36 -0500

    Actualziación desarrollo

--- src/Shared/masterPage.js
+++ src/Shared/masterPage.js
@@ -4,46 +4,10 @@ import './masterPage.css';
 import { Container, Header, Content, Footer, Sidebar, Navbar, Nav, Dropdown, Icon, Sidenav } from 'rsuite';
 import * as FaIcons from 'react-icons/fa';
 import styled from "styled-components";
-import 'boxicons'
 
 //Elemts
 // import Footer from '../Componentes/Elements/Footer/Footer';
 import {LoginAction_CerrarSesion} from '../Acciones/Login/LoginAction';
-
-/*===== SHOW NAVBAR  =====*/ 
-const showNavbar = (toggleId, navId, bodyId, headerId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId),
-    bodypd = document.getElementById(bodyId),
-    headerpd = document.getElementById(headerId)
-
-    // Validate that all variables exist
-    if(toggle && nav && bodypd && headerpd){
-        toggle.addEventListener('click', ()=>{
-            // show navbar
-            nav.classList.toggle('show')
-            // change icon
-            toggle.classList.toggle('bx-x')
-            // add padding to body
-            bodypd.classList.toggle('body-pd')
-            // add padding to header
-            headerpd.classList.toggle('body-pd')
-        })
-    }
-}
-
-showNavbar('header-toggle','nav-bar','body-pd','header')
-
-/*===== LINK ACTIVE  =====*/ 
-const linkColor = document.querySelectorAll('.nav__link')
-
-function colorLink(){
-    if(linkColor){
-        linkColor.forEach(l=> l.classList.remove('active'))
-        this.classList.add('active')
-    }
-}
-linkColor.forEach(l=> l.addEventListener('click', colorLink))
 
 export const masterPage =  (ComposedComponent) => {      
     
@@ -54,7 +18,7 @@ export const masterPage =  (ComposedComponent) => {
             this.state = {
               expand: false,
               navbar: {
-                width:'100%'
+                width:'97%',
               }
             };
             this.handleToggle = this.handleToggle.bind(this);
@@ -70,7 +34,7 @@ export const masterPage =  (ComposedComponent) => {
                 }
             }else{
                 navbarNew = {
-                    width:'100%'
+                    width:'97%'
                 }
             }
             
@@ -88,69 +52,86 @@ export const masterPage =  (ComposedComponent) => {
             return (
               <div className="show-fake-browser sidebar-page">
                 <Container>
-                    <header className="header" id="header">
-                        <div className="header__toggle">
-                            <i className='bx bx-menu' id="header-toggle"></i>
-                        </div>
-
-                        <div className="header__img">
-                            {/* <i className='bx bx-menu' id="header-toggle"></i> */}
-                            <i className='fas fa-user'/>
-                        </div>
-                    </header>
-
-                    <div className="l-navbar" id="nav-bar">
-                        <nav class="nav">
-                            <div>
-                                <ul href="#" className="nav__logo">
-                                    <i class='bx bx-layer nav__logo-icon'></i>
-                                    <span class="nav__logo-name">Bedimcode</span>
-                                </ul>
-
-                                <div className="nav__list">
-                                    <ul href="#" className="nav__link active">
-                                    <i className='bx bx-grid-alt nav__icon' ></i>
-                                        <span className="nav__name">Dashboard</span>
-                                    </ul>
-
-                                    <ul href="#" className="nav__link">
-                                        <i className='bx bx-user nav__icon' ></i>
-                                        <span className="nav__name">Users</span>
-                                    </ul>
-                                    
-                                    <ul href="#" className="nav__link">
-                                        <i class='bx bx-message-square-detail nav__icon' ></i>
-                                        <span className="nav__name">Messages</span>
-                                    </ul>
-
-                                    <ul href="#" className="nav__link">
-                                        <i class='bx bx-bookmark nav__icon' ></i>
-                                        <span className="nav__name">Favorites</span>
-                                    </ul>
-
-                                    <ul href="#" className="nav__link">
-                                        <i class='bx bx-folder nav__icon' ></i>
-                                        <span className="nav__name">Data</span>
-                                    </ul>
-
-                                    <ul href="#" className="nav__link">
-                                        <i className='bx bx-bar-chart-alt-2 nav__icon' ></i>
-                                        <span className="nav__name">Analytics</span>
-                                    </ul>
+                    <Sidebar
+                        style={{ display: 'flex', flexDirection: 'column', height:'100%'}}
+                        width={expand ? '15%' : '2%'}
+                        collapsible
+                    >
+                        <Sidenav expanded={expand} className="sidebar-body">
+                            <Nav>
+                                <Nav.Item eventKey="1" active icon={<Icon icon="avatar" style={{color:'#FFF'}} />}>
+                                    <span className="sidebar-header-labels"> OL Software</span>
+                                </Nav.Item>
+                                <hr style={{marginLeft:'5%', marginRight:'5%', width:'90%'}}/>
+                                <Nav.Item eventKey="2" active icon={<Icon icon="book" style={{color:'#FFF'}} />}>
+                                    <span className="sidebar-body-labels">Programación</span>
+                                </Nav.Item>
+                                <Nav.Item eventKey="3" active icon={<Icon icon="task" style={{color:'#FFF'}} />}>
+                                    <span className="sidebar-body-labels">Gestión de Operaciones</span>
+                                </Nav.Item>
+                                <Nav.Item eventKey="4" active icon={<Icon icon="sliders" style={{color:'#FFF'}} />}>
+                                    <span className="sidebar-body-labels">Perfiles</span>
+                                </Nav.Item>
+                                <Nav.Item eventKey="5" active icon={<Icon icon="character-authorize" style={{color:'#FFF'}} />}>
+                                    <span className="sidebar-body-labels">Roles</span>
+                                </Nav.Item>
+                                <Nav.Item eventKey="6" active icon={<Icon icon="group" style={{color:'#FFF'}} />}>
+                                    <span className="sidebar-body-labels">Usuarios</span>
+                                </Nav.Item>
+                                <Nav.Item eventKey="7" active icon={<Icon icon="bar-chart" style={{color:'#FFF'}} />}>
+                                    <span className="sidebar-body-labels">Reportes</span>
+                                </Nav.Item>
+                            </Nav>
+                        </Sidenav>
+                    </Sidebar>
+        
+                    <Container>
+                        <Header>
+                            <Nav  style={{backgroundColor:'#fff', height:'60px', display:'fixed'}}>
+                                <div className="navbar_container">
+                                <div className='sidebar-container'>
+                                    <div className='content-left'>
+                                        <Nav pullRight>
+                                            <Nav.Item onClick={this.handleToggle}  style={{ width: 56, textAlign: 'center' }}>
+                                                <Icon icon={expand ? 'angle-left' : 'bars'}  style={{color: '#1d43ad'}}/>
+                                            </Nav.Item>
+                                        </Nav>
+                                    </div>
+                                    <div className='content-right'>
+                                        <div className='right-container'>
+                                        <svg 
+                                            xmlns="http://www.w3.org/2000/svg" 
+                                            width="40" height="40" viewBox="0 0 32 32"
+                                        >
+                                            <g transform="translate(-1093 -16.291)">
+                                            <circle style={{fill:'#E7E7EE'}} cx="16" cy="16" r="16" transform="translate(1093 16.291)"/>
+                                            <path style={{fill:'#3b3798'}} d="M133.942,21.313a3.949,3.949,0,1,0-3.955-3.955A3.956,3.956,0,0,0,133.942,21.313Zm0-6.867a2.912,2.912,0,1,1-2.912,2.912A2.92,2.92,0,0,1,133.942,14.446Z" transform="translate(974.749 11.139)"/><path className="b" d="M.522,346.41H14.256a.518.518,0,0,0,.522-.522,4.968,4.968,0,0,0-4.962-4.962H4.962A4.968,4.968,0,0,0,0,345.888.518.518,0,0,0,.522,346.41Zm4.441-4.441H9.815a3.916,3.916,0,0,1,3.882,3.4H1.08A3.927,3.927,0,0,1,4.962,341.969Z" transform="translate(1101.326 -307.782)"/></g></svg>
+                                        <div>
+                                            <p className='nombre-user'>Prueba</p>
+                                        </div>
+                                        <svg 
+                                            onClick={() => {
+                                            LoginAction_CerrarSesion();
+                                                // history.push('/Home')
+                                            }}  
+                                            xmlns="http://www.w3.org/2000/svg" 
+                                            width="30" 
+                                            height="30"
+                                            viewBox="0 0 24 24" 
+                                            style={{fill:'#E7E7EE'}}><g transform="translate(0 -0.85)"><g transform="translate(0 0.85)"><g transform="translate(0 0)"><path className="a" d="M11.444,22.85H2.861a.978.978,0,0,1-.954-1v-18a.978.978,0,0,1,.954-1h8.583a.977.977,0,0,0,.954-1,.977.977,0,0,0-.954-1H2.861A2.937,2.937,0,0,0,0,3.85v18a2.937,2.937,0,0,0,2.861,3h8.583a1,1,0,0,0,0-2Z" transform="translate(0 -0.85)"/></g></g><g transform="translate(8.665 6.174)"><path className="a" d="M185.151,113.161l-5.8-5.722a.954.954,0,0,0-1.339,1.358l4.144,4.089h-11.1a.954.954,0,0,0,0,1.907h11.1l-4.144,4.089a.954.954,0,1,0,1.339,1.358l5.8-5.722a.953.953,0,0,0,0-1.358Z" transform="translate(-170.1 -107.165)"/></g></g></svg>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-
-                            <ul href="#" className="nav__link">
-                                <i className='bx bx-log-out nav__icon' ></i>
-                                <span className="nav__name">Log Out</span>
-                            </ul>
-                        </nav>
-                    </div>
-                    <Content>
-                        {/* <div className='body-container'> */}
-                            <ComposedComponent {...this.props} />
-                        {/* </div> */}
-                    </Content>
+                                </div>
+                                
+                            </Nav>
+                        </Header>
+                        <Content>
+                            {/* <div className='body-container'> */}
+                                <ComposedComponent {...this.props} />
+                            {/* </div> */}
+                        </Content>
+                    </Container>
                 </Container>
               </div>
             );
