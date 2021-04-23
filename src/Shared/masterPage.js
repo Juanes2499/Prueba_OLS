f75bcb6d7b612c08de6f4cee5e659d54df90baf3
Juan Esteban Nichoy L <juan.nichoy@mawesi.net>
2021-04-23 11:51:45 -0500

    Actualización

--- src/Shared/masterPage.js
+++ src/Shared/masterPage.js
@@ -166,7 +166,7 @@ export const masterPage =  (ComposedComponent, ...props) => {
                                         <svg 
                                             onClick={() => {
                                             LoginAction_CerrarSesion();
-                                                history.push('/Home')
+                                                //history.push('/Home')
                                             }}  
                                             xmlns="http://www.w3.org/2000/svg" 
                                             width="30" 
